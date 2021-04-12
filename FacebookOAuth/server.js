8102058e24aafcf514e8da104de462877a3e8887
Shubham Verma <lpushubham98@gmail.com>
2021-04-12 17:36:45 +0530

    commit

--- FacebookOAuth/server.js
+++ FacebookOAuth/server.js
@@ -10,6 +10,11 @@ app.get("/", (req, res) => {
 	res.render("fblogin.ejs");
 });
 
+app.get("/profile", (req, res, next) => {
+	res.render("profile.ejs");
+	next();
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running at Port:${PORT}`);
 });
